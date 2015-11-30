5fefca317281f91491b1cb15dbe4292658c27444
Kozo Nishida <knishida@riken.jp>
2015-11-30 14:47:47 +0900

    give up es6

--- test/test.js
+++ test/test.js
@@ -1,7 +1,23 @@
-let chai = require('chai');
+var KeggClient = require('../');
 
-var expect = require('chai').expect;
-expect(1).to.be.a('number');
+require('mocha')
+var assert = require("chai").assert;
 
-let assert = require('chai').assert;
-assert.typeOf(1, 'number');
+describe('keggclient rest', function(){
+	this.timeout(5000);
+	
+	it("Exists", function(){
+		assert.isDefined(KeggClient);
+	})
+	
+	describe('data ret', function () {
+		var delay = 50;
+		var org = "eco";
+		it("ret eco path from kegg", function(done){
+			KeggClient.listPathway(org, function(data){
+				console.log(data);
+				setTimeout(done, delay);
+			});
+		});
+	});
+});
