4842a92e644f7135852b4f274c373f3b148a2160
Kozo Nishida <knishida@riken.jp>
2015-12-03 07:22:46 +0900

    add conv function

--- test/test.js
+++ test/test.js
@@ -4,20 +4,33 @@ require('mocha')
 var assert = require("chai").assert;
 
 describe('keggclient rest', function(){
-	this.timeout(5000);
+	this.timeout(50000);
 	
 	it("Exists", function(){
 		assert.isDefined(KeggClient);
 	})
 	
-	describe('data ret', function () {
+	// describe('data ret', function () {
+	// 	var delay = 50;
+	// 	var org = "eco";
+	// 	it("ret eco path from kegg", function(done){
+	// 		KeggClient.listPathway(org, function(data){
+	// 			console.log(data);
+	// 			setTimeout(done, delay);
+	// 		});
+	// 	});
+	// });
+	
+	describe('conv', function () {
 		var delay = 50;
-		var org = "eco";
-		it("ret eco path from kegg", function(done){
-			KeggClient.listPathway(org, function(data){
+		var src = "eco";
+		var tgt = "ncbi-geneid";
+		it("conv eco to ncbi-geneid", function(done) {
+			KeggClient.conv(src, tgt, function(data) {
 				console.log(data);
 				setTimeout(done, delay);
 			});
 		});
 	});
+	
 });
